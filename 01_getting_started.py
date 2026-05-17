f3fb725cfeab42864f319ee4f52280cc2fa3e545
lvuser <lvuser@users.noreply.github.com>
2026-05-17 22:22:22 +0200

    updated to a loop with results printed

--- 01_getting_started.py
+++ 01_getting_started.py
@@ -7,9 +7,16 @@ def get_customer_ids() -> list[str]:
     return [f"customer{n}" for n in random.choices(range(100), k=10)]
 
 @task
+def process_customer_item(customer_id: str, id: int) -> str:
+    # Process a single customer
+    return f"Processed {customer_id}, item {id}"
+
+@task
 def process_customer(customer_id: str) -> str:
     # Process a single customer
-    return f"Processed {customer_id}"
+    i1 = process_customer_item(customer_id, 1)
+    i2 = process_customer_item(customer_id, 2)
+    return f"Processed {customer_id}: {i1} and {i2}"
 
 @flow
 def main() -> list[str]:
@@ -18,6 +25,7 @@ def main() -> list[str]:
     results = []
     for i in customer_ids:
         results.append(process_customer(i))
+    print(f"Results: {results}")
     return results
 
 
